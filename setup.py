33db72e7523f67b9b8574d03cf32305cd29bd103
Omar Crosby <omar.crosby@gmail.com>
2025-04-08 06:22:22 -0400

    chore: wip

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import os
+from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
+from InquirerPy import prompt
+
+VERSION = '0.1.0'
 
 def render_template(template_path, output_path, context):
     env = Environment(loader=FileSystemLoader(os.path.dirname(template_path)))
@@ -9,19 +13,30 @@ def render_template(template_path, output_path, context):
         file.write(content)
 
 def main():
-    project_name = input("Enter the project name: ")
-    author_name = input("Enter the author name: ")
-    description = input("Enter the project description: ")
+    questions = [
+        {"type": "input", "name": "project_name", "message": "Enter the project name:"},
+        {"type": "input", "name": "author", "message": "Enter the author name:"},
+        {"type": "input", "name": "description", "message": "Enter the project description:"},
+        {"type": "input", "name": "license", "message": "Enter the license type (e.g., MIT):"},
+        {"type": "input", "name": "email", "message": "Enter the author email:"},
+    ]
 
-    context = {
-        "project_name": project_name,
-        "author_name": author_name,
-        "project_description": description
-    }
+    context = prompt(questions)
+
+    # Store both original and modified project names
+    context['version'] = VERSION
+    context['year'] = str(datetime.now().year) # Set the current year dynamically
+    context['module_name'] = context['project_name'].lower().replace(" ", "-")  # For PyPI
+    context['package_name'] = context['project_name'].lower().replace(" ", "_")  # For internal use
 
     templates_to_render = {
-        'README.md.jinja': 'README.md',
-        'setup.py.jinja': 'setup.py',
+        'templates/LICENSE.jinja2': 'LICENSE',
+        'templates/package.json.jinja2': 'package.json',
+        'templates/pyproject.toml.jinja2': 'pyproject.toml',
+        'templates/pytest.ini.jinja2': 'pytest.ini',
+        'templates/README.md.jinja2': 'README.md',
+        'templates/release.config.js.jinja2': 'release.config.js',
+        'templates/update_version.py.jinja2': 'update_version.py',
         # Add other template files here
     }
 
